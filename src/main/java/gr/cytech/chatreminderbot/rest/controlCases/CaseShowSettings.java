252e2b36fa72b0f1b633775f6b40d2b009afe224
Hoodelliot <teo.tarlaslol@gmail.com>
2019-02-01 15:51:25 +0200

    so in this class i removed the setRequest and past it right in showSettings with this change i know can make the control to call 1 method and so i add a try catch because when you dont have timezone it thorws an noResultExeption so if it throws noResultExeption i can catch it and return that i didnt found any timezone specific to that user

--- src/main/java/gr/cytech/chatreminderbot/rest/controlCases/CaseShowSettings.java
+++ src/main/java/gr/cytech/chatreminderbot/rest/controlCases/CaseShowSettings.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 public class CaseShowSettings {
@@ -15,32 +16,40 @@ public class CaseShowSettings {
 
     private Request request;
 
-    public CaseShowSettings() {
-    }
-
     public Request getRequest() {
         return request;
     }
 
-    public void setRequest(Request request) {
-        this.request = request;
+    public CaseShowSettings() {
+
     }
 
-    String showSettings() {
-
-        TimeZone defaultTimezone = (TimeZone) entityManager.createQuery("SELECT t from TimeZone t where t.userid = :id").setParameter("id", "default").getSingleResult();
-
-        TimeZone myTimezone = (TimeZone) entityManager.createQuery("SELECT t from TimeZone t where t.userid = :id").setParameter("id", request.getMessage().getSender().getName()).getSingleResult();
-
-
+    String showSettings(Request request) {
+        this.request = request;
         String settingsShow = "---- Your timezone is  ---- \n";
         String settingsNoTimezoneFound = "---- No Timezone found default timezone is ---- \n";
         String settingsDefaultTimezone = "--- Default timezone is --- \n";
-        if (defaultTimezone.toString().isEmpty()) {
+
+        TimeZone defaultTimezone = (TimeZone) entityManager
+                .createQuery("SELECT t from TimeZone t where t.userid = :id")
+                .setParameter("id", "default")
+                .getSingleResult();
+        try {
+
+            TimeZone myTimezone = (TimeZone) entityManager
+                    .createQuery("SELECT t from TimeZone t where t.userid = :id")
+                    .setParameter("id", request.getMessage().getSender().getName())
+                    .getSingleResult();
+
+            return settingsShow + "Timezone = " + myTimezone.toString() + "\n " + settingsDefaultTimezone +
+                    "Timezone = " + defaultTimezone.toString();
+
+        } catch (NoResultException e) {
+
             return settingsNoTimezoneFound + "Timezone = " + defaultTimezone.toString();
-        } else {
-            return settingsShow + "Timezone = " + myTimezone.toString() + "\n " + settingsDefaultTimezone + "Timezone = " + defaultTimezone.toString();
+
         }
+
     }
 
 
