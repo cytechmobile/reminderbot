493a107165d3bd4dedef0ffbf50e9ad68c7583bd
Hoodelliot <teo.tarlaslol@gmail.com>
2019-02-12 12:08:17 +0200

    checks if the default timezone exists if not create one to athens and add it to database

--- src/main/java/gr/cytech/chatreminderbot/rest/controlCases/CaseShowTimezones.java
+++ src/main/java/gr/cytech/chatreminderbot/rest/controlCases/CaseShowTimezones.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 public class CaseShowTimezones {
     private final static Logger logger = LoggerFactory.getLogger(CaseShowReminders.class.getName());
@@ -23,19 +24,30 @@ public class CaseShowTimezones {
     public CaseShowTimezones() {
 
     }
+    public boolean defaultTimezoneExists() {
+        return entityManager.createQuery("SELECT t from TimeZone t where t.userid = :default")
+                .setParameter("default","default")
+                .getResultList().size() == 1;
+    }
 
-
+    @Transactional
     String showTimezones(Request request) {
         this.request = request;
         String showTimezone = "---- Your timezone is  ---- \n";
         String noTimezoneFound = "---- No Timezone found default timezone is ---- \n";
         String defaultTimezone = "---- Default timezone is ---- \n";
 
+        if (!defaultTimezoneExists()){
+            logger.info("created default timezone");
+            TimeZone timeZone = new TimeZone("Europe/Athens","default");
+            entityManager.persist(timeZone);
 
+        }
+
+        TimeZone defaultTimezoneQuery = (TimeZone) entityManager.createNamedQuery("show.timezones")
+                .setParameter("id","default")
+                .getSingleResult();
         try {
-            TimeZone defaultTimezoneQuery = (TimeZone) entityManager.createNamedQuery("show.timezones")
-                    .setParameter("id","default")
-                    .getSingleResult();
 
             TimeZone myTimezone = (TimeZone) entityManager
                     .createNamedQuery("show.timezones")
@@ -46,10 +58,7 @@ public class CaseShowTimezones {
                     "Timezone = " + defaultTimezoneQuery.toString();
 
         } catch (NoResultException e) {
-            TimeZone defaultTimezoneQuery = (TimeZone) entityManager.createNamedQuery("show.timezones")
-                    .setParameter("id","default")
-                    .getSingleResult();
-
+            logger.info("in case no timezone found for the user");
             return noTimezoneFound + "Timezone = " + defaultTimezoneQuery.toString();
 
         }
