d50092ce194db817ad8448b5cb5b54828050db0e
Hoodelliot <teo.tarlaslol@gmail.com>
2019-05-07 16:41:53 +0300

    adding shebang line to shell file

--- VersionFromPom.sh
+++ VersionFromPom.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 version=$(grep version pom.xml | grep -v '<?xml' | grep '<version>'|head -n 1|awk '{print $1}'| cut -d'>' -f 2 | cut -d'<' -f 1)
 echo "##vso[task.setvariable variable=version]$version"
